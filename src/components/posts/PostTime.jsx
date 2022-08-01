dfdebbb8de6cf291b18c50bdc5cbdbf725586941
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-08-01 15:14:42 +0300

    refined post time component

--- src/components/posts/PostTime.jsx
+++ src/components/posts/PostTime.jsx
@@ -1,7 +1,6 @@
 import { Text } from '@mantine/core'
 import React from 'react'
 
-const INITIAL_SECONDS = 120;
 const SECONDS_TO_MINUTES = 60;
 const SECONDS_TO_HOURS = 3600;
 const SECONDS_TO_DAYS = 86400;
@@ -9,27 +8,35 @@ const SECONDS_TO_WEEKS = 604800;
 const SECONDS_TO_MONTHS = 2419200;
 const SECONDS_TO_YEARS = 29030400;
 
-export default function PostTime({createdAt}) {
-    const secondsAgo = (Date.now() - createdAt) / 1000;
+const optionalS = value => value == '1' ? '' : 's';
 
+function getTime(seconds) {
     let time;
-    if (secondsAgo < INITIAL_SECONDS) time = 'Just now'
-    else if (secondsAgo < SECONDS_TO_HOURS) time = `${(secondsAgo / SECONDS_TO_MINUTES).toFixed()} minutes ago`
-    else if (secondsAgo < SECONDS_TO_DAYS) time = `${(secondsAgo / SECONDS_TO_HOURS).toFixed()} hours ago`
-    else if (secondsAgo < SECONDS_TO_WEEKS) time = `${(secondsAgo / SECONDS_TO_DAYS).toFixed()}d ago`
-    else if (secondsAgo < SECONDS_TO_MONTHS) time = `${(secondsAgo / SECONDS_TO_WEEKS).toFixed()}w ago`
-    else if (secondsAgo < SECONDS_TO_YEARS) time = `${(secondsAgo / SECONDS_TO_MONTHS).toFixed()}m ago`
-    else time = `${(secondsAgo / SECONDS_TO_YEARS).toFixed()}y ago`
 
-  return (
-      <Text sx={{
-          fontSize: 12,
-          color: '#65676B',
-          '@media (max-width: 550px)': {
-              marginTop: 10
-          },
-      }}>
-          {time}
-      </Text>
-  )
+    if (seconds < SECONDS_TO_MINUTES) time = 'Just now'
+    else if (seconds < SECONDS_TO_HOURS) time = `${(seconds / SECONDS_TO_MINUTES).toFixed()} minute${optionalS((seconds / SECONDS_TO_MINUTES).toFixed())} ago`
+    else if (seconds < SECONDS_TO_DAYS) time = `${(seconds / SECONDS_TO_HOURS).toFixed()} hour${optionalS((seconds / SECONDS_TO_HOURS).toFixed())} ago`
+    else if (seconds < SECONDS_TO_WEEKS) time = `${(seconds / SECONDS_TO_DAYS).toFixed()}d ago`
+    else if (seconds < SECONDS_TO_MONTHS) time = `${(seconds / SECONDS_TO_WEEKS).toFixed()}w ago`
+    else if (seconds < SECONDS_TO_YEARS) time = `${(seconds / SECONDS_TO_MONTHS).toFixed()}m ago`
+    else time = `${(seconds / SECONDS_TO_YEARS).toFixed()}y ago`
+
+    return time
+}
+
+export default function PostTime({ createdAt }) {
+    const secondsAgo = (Date.now() - createdAt) / 1000;
+    const time = getTime(secondsAgo);
+
+    return (
+        <Text sx={{
+            fontSize: 12,
+            color: '#65676B',
+            '@media (max-width: 550px)': {
+                marginTop: 10
+            },
+        }}>
+            {time}
+        </Text>
+    )
 }
