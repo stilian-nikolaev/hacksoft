79dbcf5ffff778861450d2b2a99c6d28370e4d76
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-08-02 20:00:34 +0300

    fixed typo in auth store

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -1,18 +1,18 @@
 import { makeObservable, observable, action } from "mobx"
 
 const TOKEN_ITEM_NAME = 'token';
-const EXPIRESAT_ITEM_NAME = 'token';
-const PROFILEID_ITEM_NAME = 'token';
+const EXPIRESAT_ITEM_NAME = 'expiresAt';
+const PROFILEID_ITEM_NAME = 'profileId';
 
 function retrieveStoredToken() {
     const storedToken = localStorage.getItem(TOKEN_ITEM_NAME)
-    const storedExpiresAt = localStorage.getItem(TOKEN_ITEM_NAME)
+    const storedExpiresAt = localStorage.getItem(EXPIRESAT_ITEM_NAME)
 
     const remainingTime = storedExpiresAt - Date.now()
     console.log(remainingTime);
     if (remainingTime <= 60000) {
         localStorage.removeItem(TOKEN_ITEM_NAME)
-        localStorage.removeItem(TOKEN_ITEM_NAME)
+        localStorage.removeItem(EXPIRESAT_ITEM_NAME)
         return null;
     }
 
@@ -53,8 +53,8 @@ class AuthStoreImpl {
         const remianingTimeInMilliseconds = expiresIn * 1000;
 
         localStorage.setItem(TOKEN_ITEM_NAME, token)
-        localStorage.setItem(TOKEN_ITEM_NAME, Date.now() + remianingTimeInMilliseconds)
-
+        localStorage.setItem(EXPIRESAT_ITEM_NAME, Date.now() + remianingTimeInMilliseconds)
+        console.log(remianingTimeInMilliseconds);
         this.logoutTimer = setTimeout(this.logout, remianingTimeInMilliseconds)
     }
 
