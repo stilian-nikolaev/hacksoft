26d1b0dea64a555fa615a3396d6438fc2d641856
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-08-02 16:31:29 +0300

    fixed css error

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 
 export default function ProfileInfo({name, occupation, imageURL}) {
     return (
-        <Box sx={{ height: 70, display: 'flex', padding: '5px 5px 10px 10px' }}>
+        <Box sx={{ height: 70, display: 'flex', justifyContent: 'space-between', padding: '5px 5px 10px 10px' }}>
+            <Box sx={{ display: 'flex' }}>
             <Avatar src={imageURL} sx={{ alignSelf: 'flex-end' }} size={50} radius="50%" alt="Avatar" />
             <Box sx={{ marginLeft: 10, alignSelf: 'center' }}>
                 <Text sx={{ fontSize: 16, fontWeight: 500 }}>{name}</Text>
                 <Text sx={{ fontSize: 13, color: '#65676B' }}>{occupation}</Text>
             </Box>
+            </Box>
             <Box sx={{ display: 'flex' }} >
-                <Image sx={{ position: 'absolute', width: 13 }} src="/edit-icon.svg" alt="edit-icon" />
+                <Image sx={{  width: 13 }} src="/edit-icon.svg" alt="edit-icon" />
             </Box>
         </Box>
     )
